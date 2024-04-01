0ed17e6a94017d25feec9be2603a9ad5a4c35d39
Nils <schoenfeld_nils@gmx.de>
2024-04-01 14:15:48 +0200

    added Throwable animations

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,12 +1,30 @@
 class ThrowableObject extends MovableObject {
 
+    IMAGES_ROTATE = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
+    ];
+    IMAGES_SPLASH = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
+    ]
+
     constructor(x, y){
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.IMAGES_ROTATE)
         this.posX = x;
         this.posY = y;
         this.height = 100;
         this.width = 100;
+        this.rotationAngle = 0; // Startwinkel
         this.throw();
+        
     }
 
     throw() {
@@ -15,8 +33,18 @@ class ThrowableObject extends MovableObject {
 
         setInterval(() => {
             this.posX += 8;
+            
+
         }, 10);
 
+        setInterval(() => {
+           this.playAnimation(this.IMAGES_ROTATE);
+        }, 80);
+
     }
+
+    
+    
+
 
 }
